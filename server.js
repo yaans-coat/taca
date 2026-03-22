5f1a831d5063aebe3919e5106ce0fb74ab8e1fca
yaans-coat <yawning.yaans@gmail.com>
2026-03-22 11:22:46 +1100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+﻿const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/tunnel', (req, res, next) => {
+    const targetUrl = req.query.url;
+    if (!targetUrl) return res.status(400).send('error: no url provided.');
+
+    createProxyMiddleware({
+        target: targetUrl,
+        changeOrigin: true,
+        followRedirects: true,
+        pathRewrite: { '^/tunnel': '' },
+        onProxyRes: (proxyRes) => {
+            delete proxyRes.headers['x-frame-options'];
+            delete proxyRes.headers['content-security-policy'];
+        },
+        onError: (err, req, res) => {
+            res.status(500).send('taca_error: connection failed.');
+        }
+    })(req, res, next);
+});
+
+app.listen(PORT, () => {
+    console.log('taca_system: active on port ' + PORT);
+});
